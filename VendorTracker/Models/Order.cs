5a74c4bfc50ced5484dda47585c11a885d854ef3
Ryan Walker <ryandanielwalker@gmail.com>
2021-05-14 09:54:33 -0700

    Pass order constructor test

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -8,11 +8,18 @@ namespace VendorTracker.Models
     public string Description { get; set; }
     public int Price { get; set; }
     public int Date { get; set; }
+    public int Id { get; }
     private static List<Order> _instances = new List<Order> { };
 
-    // public Order()
-    // {
+    public Order(string orderTitle)
+    {
+      Title = orderTitle;
+      // Description = orderDescription;
+      // Price = orderPrice;
+      // Date = orderDate;
+      // _instances.Add(this);
+      // Id = _instances.Count;
+    }
 
-    // }
   }
 }
